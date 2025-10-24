551078a4ba1482c95d69288d60b99ca92d1c68eb
El-cacahu3te <elia.nicolo@outlook.com>
2025-10-24 18:33:00 +0200

    correction des tables sql dans Database.php

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -1,9 +1,12 @@
 <?php
-
+class Database implements DatabaseInterface
+{
     const DATABASE_CONFIGURATION_FILE = __DIR__ . '/../config/database.ini';
 
     private $pdo;
 
+    public function __construct()
+    {
         // Documentation : https://www.php.net/manual/fr/function.parse-ini-file.php
         $config = parse_ini_file(self::DATABASE_CONFIGURATION_FILE, true);
 
@@ -33,12 +36,39 @@
         $stmt->execute();
 
         // Création de la table `users` si elle n'existe pas
+        $sqlUser = "CREATE TABLE IF NOT EXISTS user (
             id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL, 
+            email VARCHAR(255) NOT NULL UNIQUE,
+            birthdate DATE NOT NULL,
+            bio TEXT, 
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );";
+
+        $stmt = $this->pdo->prepare($sqlUser);
 
         $stmt->execute();
 
+        // Création de la table `games` si elle n'existe pas
+        $sqlGame = "CREATE TABLE IF NOT EXISTS game (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            release_date DATE NOT NULL,
+            game_min_age INTEGER NOT NULL,
+            has_single_player BOOLEAN DEFAULT FALSE,
+            has_multiplayer BOOLEAN DEFAULT FALSE,
+            has_coop BOOLEAN DEFAULT FALSE,
+            has_pvp BOOLEAN DEFAULT FALSE
+        );";
 
+        $stmt = $this->pdo->prepare($sqlGame);
+
+        $stmt->execute();
     }
 
+    public function getPdo(): PDO
+    {
         return $this->pdo;
     }
+}
