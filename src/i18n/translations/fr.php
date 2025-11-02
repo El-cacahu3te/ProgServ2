f4c2c87eaf465cacb675c29ed3844912502afb93
loannjui <loann.juillerat@heig-vd.ch>
2025-11-02 10:56:50 +0100

    Update fr.php

--- src/i18n/translations/fr.php
+++ src/i18n/translations/fr.php
@@ -5,7 +5,8 @@ return [
     'choose_language' => 'Choisissez votre langue préférée:',
     'game_name' => 'Nom du jeu',
     'release_date' => 'Date de sortie',
-    'minimum_age' => 'Age minimum',
+    'game_min_age' => 'Age minimum',
+    'studio_name' => 'Nom du studio',
     'languages' => [
         'en' => 'Anglais (English)',
         'fr' => 'Français',
