6cf509dc9b52eea616f94d56c40a3b88848b75d3
Marike Platen <marike.platen@heig-vd.ch>
2025-11-22 11:25:45 +0100

    implement mail in index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php
 
-use Managers\GamesManager;
-
+require_once __DIR__ . '/../src/utils/autoloader.php';
 require_once __DIR__ . '/../src/classes/Managers/GamesManager.php';
-
 const DATABASE_CONFIGURATION_FILE = __DIR__ . '/../src/config/database.ini';
+const MAIL_CONFIGURATION_FILE = __DIR__ . '/../src/config/mail.ini';
 require_once __DIR__ . '/../src/i18n/load-translation.php';
+
+use Managers\GamesManager;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 // GESTION DES COOKIES
 // Constantes
@@ -27,6 +30,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit;
 }
 
+//GESTION DES MAILS
+$config = parse_ini_file(MAIL_CONFIGURATION_FILE, true);
+
+if (!$config) {
+   throw new Exception("Erreur lors de la lecture du fichier de configuration : " .
+         MAIL_CONFIGURATION_FILE);
+}
+
+$host = $config['host'];
+$port = filter_var($config['port'], FILTER_VALIDATE_INT);
+$authentication = filter_var($config['authentication'], FILTER_VALIDATE_BOOLEAN);
+$username = $config['username'];
+$password = $config['password'];
+$from_email = $config['from_email'];
+$from_name = $config['from_name'];
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host = $host;
+    $mail->Port = $port;
+    $mail->SMTPAuth = $authentication;
+    $mail->Username = $username;
+    $mail->Password = $password;
+    $mail->CharSet = "UTF-8";
+    $mail->Encoding = "base64";
+
+    // Expéditeur et destinataire
+    $mail->setFrom($from_email, $from_name);
+    $mail->addAddress('CHANGE_ME', 'CHANGE WITH YOUR NAME');
+
+    // Contenu du mail
+    $mail->isHTML(true);
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 //GESTION DE LA BASE DE DONNEES
 
